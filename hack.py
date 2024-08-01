fa2d73b983777df65857cfa925ed808d2d3e303b
reddyrohith49471 <145396409+reddyrohith49471@users.noreply.github.com>
2024-08-02 00:04:28 +0530

    whatsapp_automatic_msg_sender.py

--- hack.py
+++
@@ -1,2 +0,0 @@
-import pywhatkit
-pywhatkit.sendwhatmsg("+919398464892","Good night",00,00)
---
+++ whatsapp_automatic_msg_sender.py
@@ -0,0 +1,3 @@
+#install (pip install pywhatkit) in terminal or cmd
+import pywhatkit
+pywhatkit.sendwhatmsg("+919xxxxxxx","Good night",00,00)  #("number including country code","msg what you need to send",hour time(in 24hrs mode),minute time)
