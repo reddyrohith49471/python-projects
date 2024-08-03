7ce15975d01baf9b2845efedc088a1997e5d4960
reddyrohith49471 <145396409+reddyrohith49471@users.noreply.github.com>
2024-08-03 23:50:48 +0530

    whatsapp_automatic_msg_sender.py

--- whatsapp_automatic_msg_sender.py
+++ whatsapp_automatic_msg_sender.py
@@ -1,3 +1,13 @@
 #install (pip install pywhatkit) in terminal or cmd
 import pywhatkit
-pywhatkit.sendwhatmsg("+919xxxxxxx","Good night",00,00)  #("number including country code","msg what you need to send",hour time(in 24hrs mode),minute time)
+from datetime import datetime
+
+now = datetime.now()
+
+chour = now.strftime("%H")
+mobile = input('Enter Mobile No of Receiver(Including country code) : ')
+message = input('Enter Message you wanna send : ')
+hour = int(input('Enter hour(In 24hr mode) : '))
+minute = int(input('Enter minute : '))
+
+pywhatkit.sendwhatmsg(mobile,message,hour,minute)
